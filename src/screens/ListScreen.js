a9141f79df5d15a7491aff1ec1e94b527e15468d
Paulo Vinicius Baleeiro Ferreira <paulo@jumpi.com.br>
2020-01-14 17:18:00 -0300

    commit

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,44 +1,144 @@
-import React from 'react';
-import { Text, StyleSheet, FlatList, View } from 'react-native';
-// import * as firebase from 'firebase';
+import React, { Component } from 'react';
+import { Text, StyleSheet, FlatList, View, Image, TouchableOpacity, Icon } from 'react-native';
 
-const lista = [
-  { nome: 'item 1' },
-  { nome: 'item 2' },
-  { nome: 'item 3' },
-  { nome: 'item 4' },
-  { nome: 'item 5' }
-];
+export default class Lista extends Component {
+  state = {
+    data: [
+      {id:0, sabor: "Em manutenção"},
+      {id:1, sabor: "Em manutenção"},
+      {id:2, sabor: "Em manutenção"},
+      {id:3, sabor: "Em manutenção"},
+    ],
+  };
+  
+  selectItem = ({ item }) => {
+    console.log(item);
+  }
 
-
-
-const ListScreen = ({ navigation }) => {
-  const lista = navigation.state.params;
-  console.log("TCL: ListScreen -> lista", lista)
-  // storeSomething(1, 'pizzaria');
-  return (
-    <View>
-      <Text style={styles.text}>List Screen</Text>
-      <FlatList
-        keyExtractor={key => key.id}
-        data={lista}
-        renderItem={({ item }) => {
-          return <Text style={styles.list}>Sabor: {item.sabor}</Text>
-        }}
-      />
-    </View>
+  renderItem = ({ item }) => (  
+    <TouchableOpacity onPress = {
+      (item) => {
+        this.selectItem(item);
+      }
+    } >
+      <View style={styles.listItem}>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{ uri: item.imagem }}
+        />
+        <View style={{
+          flexDirection: 'column',
+          flex: 2,        
+        }}>      
+          <Text style={{ marginLeft: 20 }}>{item.sabor.charAt(0).toUpperCase() + item.sabor.slice(1)}</Text>
+          <Text style={{ marginLeft: 20 }}>Tamanho: {item.tamanho}</Text>
+          <Text style={{ marginLeft: 20 }}>Preço: R$ {
+            item.valor.toString().split('.')[0] +
+            ',' +
+            item.valor.toString().split('.')[1].padEnd(2, '0')}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
+
+  
+
+  
+render() {
+  let lista = this.props.navigation.state.params;  
+  // const itemNumber = this.props.navigation.state.params.filter(item => item.isSelect).length;
+  if (this.state.loading) {
+    return (
+      <View style={
+        styles.loader
+      }>
+        <ActivityIndicator size="large"
+          color="purple" />
+      </View>
+    );
+  }  
+  return (
+    // <View>
+      <FlatList
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.props.navigation.state.params}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+        />
+      //   <Text style={          
+      //     styles.number          
+      //   }> { itemNumber }
+      //   </Text>
+      //   <TouchableOpacity style={          
+      //     styles.icon
+      //   }>   
+      //     <View>
+      //       <Icon
+      //         raised name="shopping-cart"
+      //         type="font-awesome"
+      //         color="#e3e3e3"    
+      //         size={30}          
+      //         onPress={  
+      //           () => this.goToStore() 
+      //         }
+      //         containerStyle={
+      //           {        
+      //             backgroundColor: "#FA7B5F"                            
+      //           }
+      //         } />
+      //     </View>
+      //   </TouchableOpacity>
+      // </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    margin: 15
-  },
   list: {
-    fontSize: 20,
-    margin: 15
-  }
-});
+    paddingHorizontal: 20,
+  },
+  listItem: {
+    flexDirection: "row",
+    flex: 1,
+    backgroundColor: '#EEE',
+    marginTop: 20,
+    padding: 30,
+  },
+})
 
-export default ListScreen;
+
+
+
+
+// const ListScreen = ({ navigation }) => {
+//   const lista = navigation.state.params;
+//   console.log("TCL: ListScreen -> lista", lista)
+//   // storeSomething(1, 'pizzaria');
+//   return (
+//     <View>
+//       <Text style={styles.text}>Cardápio</Text>
+//       <FlatList
+//         keyExtractor={key => key.id}
+//         data={lista}
+//         renderItem={({ item }) => {
+//           return <Text style={styles.list}>Sabor: {item.sabor}</Text>
+//         }}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   text: {
+//     fontSize: 30,
+//     margin: 15
+//   },
+//   list: {
+//     fontSize: 20,
+//     margin: 15
+//   }
+// });
+
+// export default ListScreen;
--- src/screens/QRCodeScreen.js
+++ src/screens/QRCodeScreen.js
@@ -14,34 +14,35 @@ export default function App({ navigation }) {
       
     });
   }
-  function storeCardapio(id, tipo, idItem, sabor, valor, tamanho) {
+  function storeCardapio(id, tipo, idItem, sabor, valor, tamanho, imagem) {
     // console.log(id, tipo, idItem, sabor, valor, tamanho)
     firebase.database().ref('restaurantes/' + id + '/tipo/'+ tipo + '/cardapio/'+ idItem).update({
       id: idItem,
       sabor: sabor,
       tamanho: tamanho,
       valor: valor,
+      imagem: imagem,
     })
   }
-
+  
   
   // storeSomething();
-  // storeCardapio(1,'pizzaria',1,'calabresa',35.50,'grande');
-  // storeCardapio(1,'pizzaria',2,'muçarela',35.50,'grande');
-  // storeCardapio(1,'pizzaria',3,'portuguesa',37.50,'grande');
-  // storeCardapio(1,'pizzaria',4,'banana e canela',35.50,'grande');
-  // storeCardapio(1,'pizzaria',5,'refrigerante',6.50,'lata 350ml');
-  // storeCardapio(2,'hamburgueria',1,'x-salada',14.00,'');
-  // storeCardapio(2,'hamburgueria',2,'x-bacon',16.50,'');
-  // storeCardapio(2,'hamburgueria',3,'x-egg',15.50,'');
-  // storeCardapio(2,'hamburgueria',4,'x-tudo',18.00,'');
-  // storeCardapio(2,'hamburgueria',5,'suco copo 300 ml',6.50,'');
-  // storeCardapio(3,'creperia',1,'presunto e queijo',16.00,'');
-  // storeCardapio(3,'creperia',2,'frango e creem cheese',15.50,'');
-  // storeCardapio(3,'creperia',3,'peito de peru e salada',17.50,'');
-  // storeCardapio(3,'creperia',4,'morango com nutella',18.00,'');
-  // storeCardapio(3,'creperia', 5,'cerveja lata',6.50,'');
-  storeCardapio(3,'creperia', 6,'taça vinho',9.50,'');
+  // storeCardapio(1,'pizzaria',1,'calabresa',35.50,'grande','https://conteudo.imguol.com.br/3d/2017/07/07/pizza-de-calabresa-charles-pizzas-1499449903329_v2_900x506.jpg');
+  // storeCardapio(1,'pizzaria',2,'muçarela',35.50,'grande','https://portuguessemmisterio.files.wordpress.com/2015/08/pizza-muc3a7arela-mussarela.jpg?w=245&h=139');
+  // storeCardapio(1,'pizzaria',3,'portuguesa',37.50,'grande','https://s2.glbimg.com/ovtvEum5xcHz1AiEI9U69J-HyeI=/0x0:400x268/1000x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2012/06/13/15/30/58/110/ppp.jpg');
+  // storeCardapio(1,'pizzaria',4,'banana e canela',35.50,'grande','https://www.receitasnestle.com.br/images/default-source/recipes/163-pizza-leite-moca-banana-receitas-nestle.jpg?sfvrsn=ece6eefc_2');
+  // storeCardapio(1,'pizzaria',5,'refrigerante',6.50,'lata 350ml','http://www.pizzariadp.com.br/imagens_prod/3288Coca%20Cola%20Lata.png');
+  // storeCardapio(2,'hamburgueria',1,'x-salada',14.00,'','http://k2lanches.com.br/wp-content/uploads/2015/12/X-SALADA-C%C3%B3pia-600x600.png');
+  // storeCardapio(2,'hamburgueria',2,'x-bacon',16.50,'','https://img.stpu.com.br/?img=https://s3.amazonaws.com/pu-mgr/default/a0R6f00001FpEwrEAF/5de52108e4b0256c1ba06a31.jpg&w=710&h=462');
+  // storeCardapio(2,'hamburgueria',3,'x-egg',15.50,'','http://burguercoke.com.br/wp-content/uploads/2016/02/X-EGG.png');
+  // storeCardapio(2,'hamburgueria',4,'x-tudo',18.00,'','http://static.pedidor.com/x-tudo-imgp-t1-52ac6d508bc391626b000005-52af202f8bc3913249000004-37091e3f42bceb182385ad978a879618.jpeg');
+  // storeCardapio(2,'hamburgueria',5,'suco copo 300 ml',6.50,'','https://cdn.midiamax.com.br/elasticbeanstalk-us-west-2-809048387867/uploads/2015/08/arquivos_noticias_2015_ago_istocksuco.jpg');
+  // storeCardapio(3,'creperia',1,'presunto e queijo',16.00,'','https://todaatual.com/wp-content/uploads/100_4029-696x522.jpg');
+  // storeCardapio(3,'creperia',2,'frango e creem cheese',15.50,'','https://sandrurs.files.wordpress.com/2012/12/a-crepe-de-frango-e-cream-cheese.jpg?w=900');
+  // storeCardapio(3,'creperia',3,'peito de peru e salada',17.50,'','https://abrilclaudia.files.wordpress.com/2019/01/screenshot-1.png');
+  // storeCardapio(3,'creperia',4,'morango com nutella',18.00,'','http://www.jornaldepiracicaba.com.br/wp-content/uploads/2019/01/boacomidafoto3.jpg');
+  // storeCardapio(3,'creperia', 5,'cerveja lata',6.50,'','https://ap.imagensbrasil.org/images/2017/11/05/skol.png');
+  // storeCardapio(3,'creperia', 6,'taça vinho',9.50,'','https://imagens-revista.vivadecora.com.br/uploads/2019/10/ta%C3%A7a-de-vinho-boudeaux.jpg');
 
   useEffect(() => {
     (async () => {
@@ -57,16 +58,27 @@ export default function App({ navigation }) {
     let dados = firebase.database().ref('restaurantes/' + dadosObtidos.id + '/tipo/' + tipo + '/cardapio');
     console.log("TCL: handleBarCodeScanned -> dados", dados)
     dados.on('value', function (snapshot) {
-      let cardapio = snapshot.val();
-      cardapio = cardapio.slice(1);
-      Alert.alert(
-        tipo,
-        'Restaurante encontrado com sucesso, redirecionando para o cardápio...',
-        [
-          { text: 'Ok', onPress: () => { navigation.navigate('Lista', cardapio) } },
-        ],
-        {cancelable: false},
-      )
+      if (snapshot.val()) {
+        let cardapio = snapshot.val();
+        cardapio = cardapio.slice(1);
+        Alert.alert(
+          tipo,
+          'Restaurante encontrado com sucesso, redirecionando para o cardápio...',
+          [
+            { text: 'Ok', onPress: () => { navigation.navigate('Lista', cardapio) } },
+          ],
+          {cancelable: false},
+        )        
+      } else {
+        Alert.alert(
+          'Erro ;(',
+          'Tivemos um problema para processar a sua solicitação. Tente novamente mais tarde.',
+          [
+          {text:'Ok', onPress: () => {navigation.navigate('Menu')}},
+          ],
+          { cancelable: false },
+        )
+      }
     })
   };
 
